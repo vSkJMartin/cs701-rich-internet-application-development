e44de3191dd8a6517f680e9dca50af42e9b4a606
vSkJMartin <martinliqiushi@gmail.com>
2024-04-03 16:21:12 -0400

    Handled query strings

--- mini-projects/mp4/pages/questions/index.js
+++ mini-projects/mp4/pages/questions/index.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import Link from 'next/link';
+import { useRouter } from "next/router";
 import Card from "@/components/Card";
+import Pagination from "@/components/Pagination";
 
 const QuestionsContainer = styled.div`
     display: flex;
@@ -17,21 +19,26 @@ const CardLink = styled.a`
 function Questions() {
     const [loading, setLoading] = useState(false);
     const [questions, setQuestions] = useState([]);
+    const [hasMore, setHasMore] = useState(false);
+
+    const router = useRouter();
+    const { page } = router.query;
 
     useEffect(() => {
         async function fetchData() {
             const data = await fetch(
-                'https://api.stackexchange.com/2.3/questions?page=1&order=desc&sort=hot&tagged= reactjs&site=stackoverflow');
+                `https://api.stackexchange.com/2.3/questions?${page ? `page=${page}&` : ''}order=desc&sort=hot&tagged= reactjs&site=stackoverflow`);
             const result = await data.json();
 
             if (result) {
                 setQuestions(result.items);
+                setHasMore(result.has_more);
                 setLoading(false);
             }
         }
 
         fetchData();
-    }, []);
+    }, [page]);
 
     return (
         <QuestionsContainer>
@@ -39,28 +46,31 @@ function Questions() {
             {loading ? (
                 <span>Loading...</span>
             ) : (
-                <div>
-                    {questions.map((question) => (
-                        <Link
-                            key={question.question_id}
-                            href={`/questions/${question.question_id}`}
-                            passHref
-                        >
-                            <CardLink>
-                                <Card
-                                    title={question.title}
-                                    views={question.view_count}
-                                    answers={question.answer_count}
-                                    ownerUserId={question.owner.user_id}
-                                    ownerProfileImage={question.owner.profile_image}
-                                    ownerDisplayName={question.owner.display_name}
-                                    ownerLink={question.owner.link}
-                                    score={question.score}
-                                />
-                            </CardLink>
-                        </Link>
-                    ))}
-                </div>
+                <>
+                    <div>
+                        {questions.map((question) => (
+                            <Link
+                                key={question.question_id}
+                                href={`/questions/${question.question_id}`}
+                                passHref
+                            >
+                                <CardLink>
+                                    <Card
+                                        title={question.title}
+                                        views={question.view_count}
+                                        answers={question.answer_count}
+                                        ownerUserId={question.owner.user_id}
+                                        ownerProfileImage={question.owner.profile_image}
+                                        ownerDisplayName={question.owner.display_name}
+                                        ownerLink={question.owner.link}
+                                        score={question.score}
+                                    />
+                                </CardLink>
+                            </Link>
+                        ))}
+                    </div>
+                    <Pagination currentPage={parseInt(page) || 1} hasMore={hasMore} />
+                </>
             )}
         </QuestionsContainer>
     );
